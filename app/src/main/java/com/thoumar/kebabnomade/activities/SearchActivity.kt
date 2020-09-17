89d63f4809baadfc115ee7e9cfb7bc9c14196b87
Thomas Oumar <thomas.oumar@abtasty.com>
2020-09-17 14:58:18 +0200

    Importing R

--- app/src/main/java/com/thoumar/kebabnomade/activities/SearchActivity.kt
+++ app/src/main/java/com/thoumar/kebabnomade/activities/SearchActivity.kt
@@ -2,6 +2,7 @@ package com.thoumar.kebabnomade.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thoumar.kebabnomade.R
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
