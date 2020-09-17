d0be48aec479fe5c92a031d9134bdd615e10546b
Thomas Oumar <thomas.oumar@abtasty.com>
2020-09-17 14:57:52 +0200

    Setting article content into the layout

--- app/src/main/java/com/thoumar/kebabnomade/activities/ArticleActivity.kt
+++ app/src/main/java/com/thoumar/kebabnomade/activities/ArticleActivity.kt
@@ -2,10 +2,24 @@ package com.thoumar.kebabnomade.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.thoumar.kebabnomade.R
+import com.thoumar.kebabnomade.entities.Article
+import kotlinx.android.synthetic.main.activity_article.*
 
 class ArticleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article)
+
+        val article: Article = intent.getParcelableExtra("ARTICLE")
+
+        Glide.with(this).load(article.cover).into(image_view_article_cover)
+
+        text_view_article_title.text = article.title
+        text_view_article_snippet.text = article.snippet
+        text_view_article_body.text = article.body
+
+
     }
 }
