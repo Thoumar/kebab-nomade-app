b4365f822ea9e6afe314aef61474483583a03a10
Thomas Oumar <thomas.oumar@abtasty.com>
2020-09-23 23:03:56 +0200

    Setting up first files for favorites database

---
+++ app/src/main/java/com/thoumar/kebabnomade/database/Favorite.kt
@@ -0,0 +1,2 @@
+package com.thoumar.kebabnomade.database
+
---
+++ app/src/main/java/com/thoumar/kebabnomade/database/FavoriteDao.kt
@@ -0,0 +1,8 @@
+package com.thoumar.kebabnomade.database
+
+//@Entity
+//data class SavedRestaurant(
+//    @PrimaryKey val uid: Int,
+//    @ColumnInfo(name = "first_name") val firstName: String?,
+//    @ColumnInfo(name = "last_name") val lastName: String?
+//)
